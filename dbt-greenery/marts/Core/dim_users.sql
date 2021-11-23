52d044d3836acb191c5e2f63ec19d75f839c2940
magants <magants@ucsc.edu>
2021-11-23 23:37:54 +0000

    dim_users update

--- dbt-greenery/marts/Core/dim_users.sql
+++ dbt-greenery/marts/Core/dim_users.sql
@@ -1,1 +1,25 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
 SELECT
+  u.user_id,
+  u.email,
+  u.first_name,
+  u.last_name,
+  p.status,
+  CASE
+    WHEN p.status = 1 THEN 'Inactive'
+    WHEN p.status = 2 THEN 'Inactive'
+    WHEN p.status = 3 THEN 'Active'
+    WHEN p.status = 4 THEN 'Inactive'
+    WHEN p.status = 5 THEN 'Inactive'
+    WHEN p.status = 6 THEN 'Inactive'
+    ELSE 'Other'
+  END AS status_label,
+  p.promo_id AS promo_id
+FROM {{ ref('stg_users') }} u
+LEFT JOIN {{ ref('stg_promos') }} p
+  ON u.id = p.id
