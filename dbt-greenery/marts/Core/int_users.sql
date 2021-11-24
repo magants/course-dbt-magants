6a4639c519e3e51ea0f0a9f1947c33eb68b2476d
magants <magants@ucsc.edu>
2021-11-24 23:41:10 +0000

    int_users.sql

--- dbt-greenery/marts/Core/int_users.sql
+++ dbt-greenery/marts/Core/int_users.sql
@@ -1,41 +1,15 @@
-    id,
-    user_id,
-    first_name,
-    last_name,
-    email,
-    phone_number,
-    created_at,
-    updated_at,
-    address_id
-FROM {{ source('tutorial', 'users') }}
-
-
-
 {{
   config(
     materialized='table'
   )
 }}
 
-    address_id
-
 SELECT
   u.user_id,
   u.first_name ||' '||u.last_name as full_name,
   u.email,
   phone_number,
-  p.status,
-  --address
-  CASE
-    WHEN p.status = 1 THEN 'Inactive'
-    WHEN p.status = 2 THEN 'Inactive'
-    WHEN p.status = 3 THEN 'Active'
-    WHEN p.status = 4 THEN 'Inactive'
-    WHEN p.status = 5 THEN 'Inactive'
-    WHEN p.status = 6 THEN 'Inactive'
-    ELSE 'Other'
-  END AS status_label,
-  p.promo_id AS promo_id
+  a.address
 FROM {{ ref('stg_users') }} u
--- take out promosLEFT JOIN {{ ref('stg_promos') }} p
-  ON u.id = p.id
+LEFT JOIN {{ ref('stg_addresses') }} a
+  ON u.address_id = p.address_id
