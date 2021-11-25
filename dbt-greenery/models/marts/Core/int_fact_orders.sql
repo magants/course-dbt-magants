b1a398f5915c5fc5961d120c9ad1eef5c7883a40
magants <magants@ucsc.edu>
2021-11-25 17:34:43 +0000

    int_facts_orders

--- dbt-greenery/models/marts/Core/int_fact_orders.sql
+++ dbt-greenery/models/marts/Core/int_fact_orders.sql
@@ -14,7 +14,12 @@ o.order_id,
     a.zipcode,
     a.state,
     a.country,
-    o.promo_id,
+    p.name as product_name,
+    p.price,
+    p.quantity,
+    pr.promo_id,
+    pr.discount,
+    pr.status,
     o.order_cost,
     o.shipping_cost,
     o.order_total,
@@ -28,8 +33,12 @@ FROM
 {{ ref('stg_orders') }} o,
 {{ ref('stg_order_items') }} oi,
 {{ ref('int_users') }} iu,
-{{ ref('stg_addresses') }} a
+{{ ref('stg_addresses') }} a,
+{{ ref('stg_products') }} p,
+{{ ref('stg_promos') }} p
 
 WHERE o.order_id = oi.order_id
 AND o.user_id = iu.user_id
 AND o.address_id=a.address_id
+AND oi.product_id=p.product_id
+AND o.promo_id=pr.promo_id
--- dbt-greenery/models/marts/Core/test.sql
+++ dbt-greenery/models/marts/Core/test.sql
@@ -8,6 +8,9 @@ SELECT
     a.zipcode,
     a.state,
     a.country,
+    p.name as product_name,
+    p.price,
+    p.quantity,
     o.promo_id,
     o.order_cost,
     o.shipping_cost,
@@ -21,8 +24,10 @@ FROM
 dbt_magants.stg_orders o,
 dbt_magants.stg_order_items oi,
 dbt_magants.int_users iu,
-dbt_magants.stg_addresses a
+dbt_magants.stg_addresses a,
+dbt_magants.stg_products p
 
 WHERE o.order_id = oi.order_id
 AND o.user_id = iu.user_id
 AND o.address_id=a.address_id
+AND oi.product_id=p.product_id
