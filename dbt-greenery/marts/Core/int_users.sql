76351bebdaa10eb3be99379301cbf21ea3ff4690
magants <magants@ucsc.edu>
2021-11-25 00:09:36 +0000

    int_users.sql

--- dbt-greenery/marts/Core/int_users.sql
+++ dbt-greenery/marts/Core/int_users.sql
@@ -9,7 +9,10 @@ SELECT
   u.first_name ||' '||u.last_name as full_name,
   u.email,
   phone_number,
-  a.address
+  a.address,
+  a.zipcode,
+  a.state,
+  a.country
 FROM {{ ref('stg_users') }} u
 LEFT JOIN {{ ref('stg_addresses') }} a
   ON u.address_id = a.address_id
